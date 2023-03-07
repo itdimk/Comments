d501266696b5592e313bd70f34d505f3f8596f94
itdimk <zechmint@gmail.com>
2023-03-07 21:28:07 +0300

    Webpack setup, dynamically adding comments

---
+++ src/index.js
@@ -0,0 +1,9 @@
+﻿import "./scss/style.scss"
+import Comment from "./comment.html"
+
+(async function () {
+    const comments = document.getElementById('comments')
+    console.log(Comment)
+    comments.insertAdjacentHTML('afterbegin', Comment)
+    comments.insertAdjacentHTML('afterbegin', Comment)
+})()
